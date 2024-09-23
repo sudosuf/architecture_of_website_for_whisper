bc7a55ceb060985398569412b7c22f2f258411e4
sudosuf <66674980+sudosuf@users.noreply.github.com>
2024-09-23 17:06:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ dict = {str: np.float32}
 device = "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "openai/whisper-tiny"
+model_id = "openai/whisper-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype
